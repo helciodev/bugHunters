a66de88e307b22e178c6a45ba87f2afe29b81c78
helciodev <helcioandre@live.com>
2021-06-30 13:48:35 +0100

    Delete SimplesScene.js and replace by GameScene.js Add config folder and config.js file

---
+++ src/config/config.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser';
+ 
+export default {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  dom: {
+    createContainer: true,
+  },
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    acrade: {
+      gravity: { y: 0 },
+      debug: true,
+    },
+  },
+};
