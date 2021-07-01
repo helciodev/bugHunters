785094ddf655872195e02d35b732b266bb0a91d6
helciodev <helcioandre@live.com>
2021-07-01 15:38:17 +0100

    Delete unnecessary webpack line

--- webpack.config.js
+++ webpack.config.js
@@ -11,21 +11,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
   },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
   },
